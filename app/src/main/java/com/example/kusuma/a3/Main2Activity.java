06cefe322301a33b30a19b95ec2d9c22daa666f9
Kusuma-G <kusum.g.gowda@gmail.com>
2019-07-04 08:25:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kusuma/a3/Main2Activity.java
@@ -0,0 +1,25 @@
+package com.example.kusuma.a3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button angrybutton2 = (Button) findViewById(R.id.angry_btn2);
+        angrybutton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this,Main3Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
